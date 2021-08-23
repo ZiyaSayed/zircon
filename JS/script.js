e8be2a31a71e24f9110dd3f31e42313a2b8e2c3d
ZiyaSayed <63648174+ZiyaSayed@users.noreply.github.com>
2021-08-23 23:22:31 +0530

    Sidebar submenu

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,13 @@
+// ----------------
+//                 Sidebar 
+//                         ---------------------------------
+
 const sidebar = document.getElementById('sidebar');
 const navContainer = document.getElementById('sidebar');
 const hamburger = document.getElementById('openSidebar');
 const closeSidebar = document.getElementById('closeSidebar');
 const page =document.querySelector('.page');
+
 
 hamburger.addEventListener("click", ()=>
 {
@@ -19,3 +24,32 @@ closeSidebar.addEventListener("click", ()=>
     navContainer.style.left='-18rem';
     navContainer.style.width="0rem";
 })
+
+// ----------------
+//                 Sidebar Submenu
+//                         ---------------------------------
+const openSubmenuIcons=document.querySelectorAll('.openSubmenu');
+const subcategoryMenu = document.querySelectorAll('.subcategoryMenu');
+openSubmenuIcons.forEach(icon => {
+    
+    icon.addEventListener("click", ()=>
+    {
+        let submenuNumber=icon.dataset.submenu;
+        let submenu =document.getElementById(`subcategoryMenu-${submenuNumber}`);
+
+        subcategoryMenu.forEach(menu => {
+                if(menu!=submenu)
+                    menu.classList.remove('showSubmenu');
+            });
+
+            icon.classList.toggle('rotate');
+            submenu.classList.toggle('showSubmenu');
+
+        openSubmenuIcons.forEach(i => 
+            {
+                if(i.dataset.submenu!=submenuNumber)
+                    i.classList.remove('rotate');
+            })
+
+    })
+});
